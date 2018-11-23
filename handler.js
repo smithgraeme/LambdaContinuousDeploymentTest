256703b19ca5eb9ab9eeefd2bda4bd37f3141809
Graeme Smith <30961431+smithgraeme@users.noreply.github.com>
2018-11-22 19:00:09 -0700

    test CD

--- handler.js
+++ handler.js
@@ -10,6 +10,7 @@ module.exports.hello = async (event, context) => {
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully! Deployed via git pipeline.',
       input: event.queryStringParameters,
+      event: event,
     }),
   };
 };
