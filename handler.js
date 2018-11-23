2b0570c0c21576927ef3a2117da15d15bae8e762
Graeme Smith <30961431+smithgraeme@users.noreply.github.com>
2018-11-22 18:57:17 -0700

    test CD

--- handler.js
+++ handler.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports.hello = async (event, context) => {
-  console.log("Hello from the serverless! Deployment worked!")
+  console.log("Hello from the serverless! Deployment worked. Deployed via git pipeline.");
 
   console.log(JSON.stringify(event.queryStringParameters));
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Your function executed successfully! Deployed via git pipeline.',
       input: event.queryStringParameters,
     }),
   };
